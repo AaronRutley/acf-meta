1e9d5f111e6f5714f0cbca93e76d15fc1f6f59c7
Aaron Rutley <aaronrutley@gmail.com>
2016-03-18 14:24:03 +1100

    refactoring and og:meta first pass

---
+++ acf_meta.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Plugin Name: ACF Meta
+Plugin URI: http://www.aaronrutley.com/
+Description: Very experimental WordPress plugin to use ACF to set the title & description meta tags.
+Version: 0.0.1
+Author: Aaron Rutley
+Author URI: http://www.aaronrutley.com/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+class ACF_Meta_Plugin {
+
+	function __construct()
+	{
+		// include meta fields
+		function acf_meta_load_field_group() {
+			require_once(__DIR__.'/acf_meta_fields.php');
+		}
+		add_action('acf/init', 'acf_meta_load_field_group');
+
+
+		// setup custom image size
+		if ( function_exists( 'add_theme_support' ) ) {
+			add_image_size( 'acf_meta_1200', 1200, '', true );
+		}
+
+		// set custom title tag
+		function acf_meta_custom_title() {
+			global $post;
+			$acf_meta_title = get_field( 'acf_meta_title', $post->ID);
+			// check if this post has a title set
+			if(!empty($acf_meta_title)) {
+				return $acf_meta_title;
+			} else {
+				// set a fallback title with a max length of 60 chars
+				$fallback_title = $post->post_title.' - '.get_bloginfo( 'name' );
+				$fallback_title_short = mb_strimwidth($fallback_title, 0, 60);
+				return $fallback_title_short;
+			}
+		}
+		add_filter( 'wp_title', 'acf_meta_custom_title', 10, 2 );
+
+
+		// set custom meta tags
+		function acf_meta_custom_tags() {
+		    global $post;
+
+			// og meta tags for site name, title and url
+			echo '<meta property="og:site_name" content="'.get_bloginfo( 'name' ).'" />' . "\n";
+			echo '<meta property="og:url" content="'.get_permalink().'" />' . "\n";
+			echo '<meta property="og:title" content="'.acf_meta_custom_title().'" />' . "\n";
+
+			// image og meta tag - if we have an image echo the tag
+			$acf_meta_image_array = get_field( 'acf_meta_image', $post->ID);
+			$acf_meta_image_url = $acf_meta_image_array['sizes']['acf_meta_1200'];
+			if(!empty($acf_meta_image_url)) {
+				echo '<meta property="og:image" content="'.$acf_meta_image_url.'" />' . "\n";
+			}
+
+			// description tag and og meta tag - if we have a description echo the tag
+			$acf_meta_description = get_field( 'acf_meta_description', $post->ID);
+			if(!empty($acf_meta_description)) {
+				echo '<meta property="og:description" content="'. $acf_meta_description . '" />' . "\n";
+		    	echo '<meta name="description" content="'. $acf_meta_description . '" />' . "\n";
+			}
+		}
+		add_action( 'wp_head', 'acf_meta_custom_tags' , 2 );
+
+	}
+}
+
+$acf_meta_plugin = new ACF_Meta_Plugin();
---
+++ acf_meta_fields.php
@@ -0,0 +1,97 @@
+<?php // partial for the acf fields used
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array (
+	'key' => 'group_56eb45e948d7a',
+	'title' => 'Meta Data',
+	'fields' => array (
+		array (
+			'key' => 'field_56eb45ecdd246',
+			'label' => 'Title',
+			'name' => 'acf_meta_title',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+			'readonly' => 0,
+			'disabled' => 0,
+		),
+		array (
+			'key' => 'field_56eb45f2dd247',
+			'label' => 'Description',
+			'name' => 'acf_meta_description',
+			'type' => 'textarea',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'maxlength' => 160,
+			'rows' => 2,
+			'new_lines' => '',
+			'readonly' => 0,
+			'disabled' => 0,
+		),
+		array (
+			'key' => 'field_56eb6f5143f6b',
+			'label' => 'Image',
+			'name' => 'acf_meta_image',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'array',
+			'preview_size' => 'medium',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '!=',
+				'value' => 'acf',
+			),
+		),
+	),
+	'menu_order' => 100,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'left',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+endif;
+
+?>
